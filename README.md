255978e4bc4b429ce03a1b47f2de051234a75ac4
Noixas <acmrz@outlook.com>
2020-06-13 15:25:41 +0200

    Bot work and get my position

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # UvA-Admission-Bot
 Small bot to check what place I am in the waiting list to get into the masters at UvA.
+
+### Ids of the website
+
+HTML ids to get the coreect field in the site.
+
+https://datanose.nl/
+Login UvAnetID id:_177 _176 is th elink i think
+
+My id: yyyyyyy
+PAssword: xxxxxx
+
+class="btn-submit
+
+Master enrolment _308
+
+Radio _393
+
+Text _463
+
+Retrieve any number or text 
+
+python -c 'import pyppeteer; pyppeteer.chromium_downloader.download_chromium()'
---
+++ bot.py
@@ -0,0 +1,82 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+my_url = 'https://datanose.nl/#'
+login ='https://content.datanose.nl/pages/login.jpg'
+driver = webdriver.Chrome('F:\chromedriver_win32\chromedriver.exe')
+driver.get(my_url)
+
+timeout = 5
+try:
+    element_present = EC.presence_of_element_located((By.CLASS_NAME, 'tile'))
+    WebDriverWait(driver, timeout).until(element_present)
+except TimeoutException:
+    print ("Timed out waiting for page to load")
+
+
+
+titles = driver.find_elements_by_class_name('tile')
+login_tile = 5
+titles[login_tile].click()
+
+
+timeout = 5
+try:
+    element_present = EC.presence_of_element_located((By.ID, 'username'))
+    WebDriverWait(driver, timeout).until(element_present)
+except TimeoutException:
+    print ("Timed out waiting for page to load")
+
+
+file5 = open('Credentials', 'r') 
+Lines = file5.readlines() 
+
+username = driver.find_element_by_id('username')
+username.send_keys(Lines[0])
+try:
+    element_present = EC.presence_of_element_located((By.ID, 'password'))
+    WebDriverWait(driver, timeout).until(element_present)
+except TimeoutException:
+    print ("Timed out waiting for page to load")
+    
+password = driver.find_element_by_id('password')
+password.send_keys(Lines[1])
+password.send_keys(Keys.ENTER)
+print('end')
+print(username)
+url2 = "nav#enrolmaster"
+try:
+    element_present = EC.presence_of_element_located((By.XPATH, '//a[@href="'+url2+'"]'))
+    WebDriverWait(driver, timeout).until(element_present)
+except TimeoutException:
+    print ("Timed out waiting for page to load")
+
+masters = driver.find_element_by_xpath('//a[@href="'+url2+'"]')
+masters.click()
+print(masters)
+print('DONE')
+
+url2 = 'radio'    
+timeout = 10
+try:    
+    element_present = EC.presence_of_element_located((By.XPATH, "//input[@type='radio']"))
+    WebDriverWait(driver, timeout).until(element_present)
+except TimeoutException:
+    print ("Timed out waiting for page to load")
+radio = driver.find_element_by_xpath("//input[@type='radio']")
+radio.click()
+uva_text = "selected and you are on the waiting list." 
+
+
+try:    
+    element_present = EC.presence_of_element_located((By.XPATH, "//*[contains(text(), '"+uva_text+"')]"))
+    WebDriverWait(driver, timeout).until(element_present)
+except TimeoutException:    
+    print ("Timed out waiting for page to load")
+texta = driver.find_element_by_xpath("//*[contains(text(), '"+uva_text+"')]")
+print(texta.text)
+position = [int(s) for s in texta.text.split() if s.isdigit()][0]
+print("Position:",position+1)
